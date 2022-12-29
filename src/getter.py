c287a8f77cf20f85934fab235076c6ae6a56df4e
Yuki <yksk171118@gmail.com>
2022-12-29 12:04:56 +0900

    基本機能の完成

--- src/getter.py
+++ src/getter.py
@@ -1,14 +1,46 @@
 # Youtubeからサムネイルを取得する
 
 import requests
+import os
 
-_base_url = 'https://img.youtube.com/vi/{0}/default.jpg'
+_base_url = 'https://img.youtube.com/vi/{0}/maxresdefault.jpg'
+_save_folder_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Pictures') + '\\Youtube_Thumbnail'
 
 # 動画IDを受け取りYoutubeからサムネイルを取得する
 def get(movieId):
+    # サムネイルを取得
+    try :
+        res = requests.get(_base_url.format(movieId))
+    except requests.exceptions.RequestException :
+        return -1
+    except Exception:
+        return -2
     
-    pass
+    #ステータスコードが200番台ではなかった場合　処理をスキップ
+    if res.status_code == 200:
+        r = _save(filename=movieId+'.png',data=res.content)
+    else :
+        return -3
+    # 処理に成功し正常に保存できた場合0を返す
+    return 0
 
-#取得したサムネイルを保存
-def _save():
-    pass
+#取得したサムネイルを保存 filename = 保存するファイル名 data = サムネイル画像のデータ本体
+def _save(*,filename,data):
+
+    if not os.path.exists(_save_folder_path):
+        os.mkdir(_save_folder_path)
+    
+    if os.path.exists(_save_folder_path + '\\'+filename):
+        print('すでに同じ名前のファイルが存在します\n上書きしますか?(y/n)')
+        i = input()
+        if i == 'n':
+            return 0
+        elif not i =='y':
+            print('Plese input y or n')
+            return 1
+        
+    # ファイルを保存
+    with open(_save_folder_path +'\\' + filename,'wb') as f:
+        f.write(data)
+
+    return 0
--- src/main.py
+++ src/main.py
@@ -1,20 +1,7 @@
 # Youtubeの動画IDを指定して起動すると指定した動画のサムネイルを取得して保存する
 
 import getter
-import sys
 
-def main():
-    args = sys.argv #コマンドライン引数を取得
-    video_ids = []
-# コマンドライン引数を作業用変数にコピー
-    if 0 == len(args):
-        video_ids.append(input())
-    else :
-        for tmp in args:
-            video_ids.append(tmp)
-    del args  #コマンドライン引数を解放
-
-    pass
-
-if __name__ == "main":
-    main() 
+print('動画IDを入力してください')
+video_id = input()
+getter.get(video_id)
