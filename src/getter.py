e40f47ac3931c05e978c5c6a0a3a735bdb7aa91f
Yuki <yksk171118@gmail.com>
2022-12-29 10:55:18 +0900

    ファイルを追加

---
+++ src/getter.py
@@ -0,0 +1,14 @@
+# Youtubeからサムネイルを取得する
+
+import requests
+
+_base_url = 'https://img.youtube.com/vi/{0}/default.jpg'
+
+# 動画IDを受け取りYoutubeからサムネイルを取得する
+def get(movieId):
+    
+    pass
+
+#取得したサムネイルを保存
+def _save():
+    pass
